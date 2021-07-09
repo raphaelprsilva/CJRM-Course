ceb1ff8d4e8da43061365f39de1607e4c7ff7e0e
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-07-09 20:02:32 -0300

    Add code implementation

--- 09-Etapa-Aplicaco-To-Do-List/00-01-To-do-list-project/app.js
+++ 09-Etapa-Aplicaco-To-Do-List/00-01-To-do-list-project/app.js
@@ -2,53 +2,72 @@ const formAddTodo = document.querySelector('.form-add-todo');
 const todosContainer = document.querySelector('.todos-container');
 const inputSearchTodo = document.querySelector('.form-search input');
 
-// console.log(Array.from(todosContainer.children)[0].innerText);
-// console.log(todosContainer.children[0].innerText);
-
-formAddTodo.addEventListener('submit', event => {
-  event.preventDefault();
-
-  // add - é a classe do input
-  // trim() - método que retira os espaços em branco antes e depois
-  const inputValue = event.target.add.value.trim();
-
+const addTodo = (inputValue) => {
   if (inputValue.length) {
     todosContainer.innerHTML += `
-      <li class="list-group-item d-flex justify-content-between align-items-center">
-        <span>${ inputValue }</span>
-        <i class="far fa-trash-alt delete"></i>
+      <li class="list-group-item d-flex justify-content-between align-items-center" data-todo="${inputValue}">
+        <span>${inputValue}</span>
+        <i class="far fa-trash-alt" data-trash="${inputValue}"></i>
       </li>
     `;
 
     event.target.reset();
   }
+};
+
+formAddTodo.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  // add - é a classe do input
+  // trim() - método que retira os espaços em branco antes e depois
+  const inputValue = event.target.add.value.trim();
+  addTodo(inputValue);
 });
 
-todosContainer.addEventListener('click', event => {
-  const clickedElement = event.target;
-  if (Array.from(clickedElement.classList).includes('delete')) {
-    clickedElement.parentElement.remove();
+const removeTodo = (clickedElement) => {
+  const trashDataValue = clickedElement.dataset.trash;
+  const todo = document.querySelector(`[data-todo="${trashDataValue}"]`);
+
+  if (trashDataValue) {
+    todo.remove();
   }
+};
+
+todosContainer.addEventListener('click', (event) => {
+  const clickedElement = event.target;
+  removeTodo(clickedElement);
 });
 
-inputSearchTodo.addEventListener('input', event => {
+const filterTodos = (todoList, inputValue, returnMatchedTodos) => {
+  return todoList.filter((listItem) => {
+    const matchedTodos = listItem.innerText.toLowerCase().includes(inputValue);
+    return returnMatchedTodos ? matchedTodos : !matchedTodos;
+  });
+};
+
+const manipulateTodos = (todoList, classToRemove, classToAdd) => {
+  todoList.forEach((todo) => {
+    todo.classList.remove(classToRemove);
+    todo.classList.add(classToAdd);
+  });
+};
+
+const hideTodos = (todoList, inputValue) => {
+  const todosToHide = filterTodos(todoList, inputValue, false);
+  manipulateTodos(todosToHide, 'd-flex', 'hidden');
+};
+
+const showTodos = (todoList, inputValue) => {
+  const todosToShow = filterTodos(todoList, inputValue, true);
+  manipulateTodos(todosToShow, 'hidden', 'd-flex');
+};
+
+inputSearchTodo.addEventListener('input', (event) => {
   event.preventDefault();
 
   const inputValue = event.target.value.trim().toLowerCase();
+  const allTodos = Array.from(todosContainer.children);
 
-  console.log(inputValue);
-
-  Array.from(todosContainer.children)
-    .filter((listItem) => !listItem.innerText.toLowerCase().includes(inputValue))
-    .forEach((todo) => {
-      todo.classList.remove('d-flex');
-      todo.classList.add('hidden');
-    });
-
-  Array.from(todosContainer.children)
-  .filter((listItem) => listItem.innerText.toLowerCase().includes(inputValue))
-  .forEach((todo) => {
-    todo.classList.remove('hidden');
-    todo.classList.add('d-flex');
-  });
-})
+  hideTodos(allTodos, inputValue);
+  showTodos(allTodos, inputValue);
+});
