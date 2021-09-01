53d4d7681020c56a1102dbc55883a7234f57a190
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-08-31 21:04:11 -0300

    Add exercises

--- 11-Etapa-Assincrono/04-03-promises/app.js
+++ 11-Etapa-Assincrono/04-03-promises/app.js
@@ -1,39 +1,36 @@
-const getDigimons = (url) =>
-  new Promise((resolve, reject) => {
+const getData = (url) => {
+  return new Promise((resolve, reject) => {
     const request = new XMLHttpRequest();
-
+    
     request.addEventListener('readystatechange', () => {
-      // Estou verificando se a propriedade 'readyState' do 'request'
-      // é igual a 4, pois, só neste estado, que posso "fazer algo"
-      // com a resposta
       const isRequestOkay = request.readyState === 4 && request.status === 200;
       const isRequestNotOkay = request.readyState === 4;
       if (isRequestOkay) {
         const data = JSON.parse(request.responseText);
         resolve(data);
       }
-
+    
       if (isRequestNotOkay) {
         reject('Não foi possível obter os dados');
       }
     });
-
+    
     request.open('GET', url);
     request.send();
-  });
+  })
+}
 
-getDigimons('https://digimon-api.vercel.app/api/digimon/name/Koromon')
-  .then((koromon) => {
-    console.log(koromon);
-    return getDigimons(
-      'https://digimon-api.vercel.app/api/digimon/name/Tsunomon'
-    );
-  })
-  .then((tsunomon) => {
-    console.log(tsunomon);
-    return getDigimons(
-      'https://digimon-api.vercel.app/api/digimon/name/Agumon'
-    );
-  })
-  .then((agumon) => console.log(agumon))
-  .catch((error) => console.log(error));
+getData('https://jsonplaceholder.typicode.com/todos/')
+  .then((todos) => console.log(todos))
+  .catch((error) => console.log(error))
+
+// const getData = () => {
+//   return new Promise((resolve, reject) => {
+//     // resolve('Dados aqui');
+//     reject('Erro aqui');
+//   })
+// };
+
+// getData()
+//   .then((value) => console.log(value))
+//   .catch((error) => console.log(error))
