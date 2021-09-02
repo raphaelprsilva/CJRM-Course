d32160a8f5df18cf49ccbcdfa6ee8c40ff13a14b
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-09-02 19:38:57 -0300

    Add project files

--- 12-Etapa-Aplicacao-Weather/js/weather.js
+++ 12-Etapa-Aplicacao-Weather/js/weather.js
@@ -2,36 +2,33 @@ const APIKey = '004dTU1swCv8D9SGILWREoq0Yv33s8fB';
 const getCityUrl = (cityUrl) =>
   `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityUrl}`;
 
-const getCityData = async (cityName) => {
+const getCityWeatherUrl = (key, APIKey) => 
+  `http://dataservice.accuweather.com/currentconditions/v1/${key}?apikey=${APIKey}&language=pt-br`;
+
+const fetchData = async (url) => {
   try {
-    const cityUrl = getCityUrl(cityName)
-    const response = await fetch(cityUrl);
+    const response = await fetch(url);
 
     if (!response.ok) {
       throw new Error('Was not possible to get data from API.')
     }
-    const [cityData] = await response.json();
-    return cityData;
+
+    return await response.json();
   } catch ({ name, message }) {
     alert(`${name}: ${message}`);
   }
-};
+}
+
+const getCityData = (cityName) => {
+  const cityUrl = getCityUrl(cityName);
+  return fetchData(cityUrl);
+}
 
 const getCityWeather = async (cityName) => {
-  try {
-    const { Key } = await getCityData(cityName);
-    const cityWeatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}`;
-    const response = await fetch(cityWeatherUrl);
-
-    if (!response.ok) {
-      throw new Error('Was not possible to get data from API.')
-    }
-    const [cityWeatherData] = await response.json();
-    debugger
-    return cityWeatherData;
-  } catch ({ name, message }) {
-    alert(`${name}: ${message}`);
-  }
+  const [cityData] = await getCityData(cityName);
+  const { Key } = cityData;
+  const response = await fetchData(getCityWeatherUrl(Key, APIKey));
+  return response;
 };
 
-getCityWeather('São Paulo');
+getCityWeather('Uberlândia');
