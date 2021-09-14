85b79c91f0b4013067ace35b7dbb4b7afbb7cd9b
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-09-14 13:09:12 -0300

    Add exercises

--- 14-Etapa-POO/01-06-exercicios-36/app.js
+++ 14-Etapa-POO/01-06-exercicios-36/app.js
@@ -1,9 +1,9 @@
 /*
-  01 
-  
-  - Atualmente, a invocação da função abaixo está retornando 
+  01
+
+  - Atualmente, a invocação da função abaixo está retornando
     "50,100,50undefinedundefined";
-  - Descubra o que o código abaixo está fazendo e faça a invocação da função 
+  - Descubra o que o código abaixo está fazendo e faça a invocação da função
     retornar 200;
   - Não use a sintaxe item[index].
 */
@@ -17,9 +17,9 @@ const sum = (x, y, z) => x + y + z;
 /*
   02
 
-  - Declare uma const que armazena seu primeiro nome com todas as letras 
+  - Declare uma const que armazena seu primeiro nome com todas as letras
     minúsculas;
-  - Utilizando (também) o spread operator, exiba no console o seu nome com 
+  - Utilizando (também) o spread operator, exiba no console o seu nome com
     apenas a primeira letra maiúscula.
 */
 
@@ -58,7 +58,7 @@ const obj = {
 /*
   04
 
-  - Descubra o que o código abaixo está fazendo e refatore-o para que o objeto 
+  - Descubra o que o código abaixo está fazendo e refatore-o para que o objeto
     criado permaneça intacto.
 */
 
@@ -79,8 +79,8 @@ const newObj = first(object);
 /*
   05
 
-  - A partir do código abaixo, crie um objeto em que as propriedades são os 
-    valores das propriedades 'date' e os valores das propriedades são os 
+  - A partir do código abaixo, crie um objeto em que as propriedades são os
+    valores das propriedades 'date' e os valores das propriedades são os
     valores de 'value'.
 
   O objeto criado deve ser assim:
@@ -119,17 +119,17 @@ const values = timestamps.reduce((accumulator, { date, value }) => {
 
   - Já implementamos os métodos some, map e filter, do zero;
   - Neste exercício, seu desafio será criar, do zero, o método forEach;
-  - Implemente uma função "forEach" que possui a mesma funcionalidade do método 
+  - Implemente uma função "forEach" que possui a mesma funcionalidade do método
     forEach original;
   - Você não poderá utilizar o método forEach original, embutido na linguagem;
-  - A invocação da função "forEach" deve receber um array como 1º argumento e a 
+  - A invocação da função "forEach" deve receber um array como 1º argumento e a
     declaração de uma função como 2º argumento;
   - Faça os testes usando a let e const abaixo;
     - Após a execução do forEach, accumulator deve armazenar 236;
-    - A cada execução do forEach, a mensagem '"X" é o Xº item do array 
-      [XX, XX, XX, XX]' deve ser exibida no console. Os "X" são placeholders 
+    - A cada execução do forEach, a mensagem '"X" é o Xº item do array
+      [XX, XX, XX, XX]' deve ser exibida no console. Os "X" são placeholders
       que devem ser preenchidos com as informações corretas.
-  
+
   Dica 1: o método forEach é usado para executar efeitos colaterais;
   Dica 2: o método forEach nunca retorna um valor.
 */
@@ -157,29 +157,29 @@ const forEach = (arr, func) => {
 /*
   07
 
-  - Implemente um carousel. O desafio é que o resultado final seja este: 
+  - Implemente um carousel. O desafio é que o resultado final seja este:
     https://youtu.be/ydn_bSwvlPU
-  - As imagens, marcação HTML e estilos CSS estão feitos. Aqui no app.js você 
+  - As imagens, marcação HTML e estilos CSS estão feitos. Aqui no app.js você
     fará os passos abaixo;
   - Quando o botão de "próximo slide" for clicado:
     1 Remova, de todos os slides, as classes que os deixam visíveis;
     2 Adicione, em todos os slides, as classes que os deixam invisíveis;
-    3 Adicione, apenas no slide que deve ser exibido, a classe que deixa um 
+    3 Adicione, apenas no slide que deve ser exibido, a classe que deixa um
       slide visível;
-      3.1 Para saber qual slide deve ficar visível, quando o arquivo .js for 
-        carregado, inicialize uma variável com 0. Ela representa o index do 
+      3.1 Para saber qual slide deve ficar visível, quando o arquivo .js for
+        carregado, inicialize uma variável com 0. Ela representa o index do
         slide exibido atualmente;
-      3.2 Antes de adicionar e remover as classes dos slides, verifique o index 
+      3.2 Antes de adicionar e remover as classes dos slides, verifique o index
         do slide exibido atualmente;
-          3.3 Se ele corresponder ao index do último slide (3º), o 1º slide deve 
+          3.3 Se ele corresponder ao index do último slide (3º), o 1º slide deve
             ser exibido;
-          3.4 Se ele não corresponder ao index do último slide, o próximo slide 
+          3.4 Se ele não corresponder ao index do último slide, o próximo slide
             deve ser exibido.
   - Quando o botão de "slide anterior" for clicado:
     1 Execute os passos 1, 2 e 3 acima;
-    2 No passo 3.3, se o slide exibido atualmente corresponder ao index do 1º 
+    2 No passo 3.3, se o slide exibido atualmente corresponder ao index do 1º
       slide, o último slide (3º) deve ser exibido;
-    3 No passo 3.4, se o slide exibido atualmente não corresponder ao index do 
+    3 No passo 3.4, se o slide exibido atualmente não corresponder ao index do
       1º slide, o slide anterior deve ser exibido.
 */
 
